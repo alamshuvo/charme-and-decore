af7b9de13c2d020eeeff7482c1af6f4d2cbbc29c
alamshuvo <alamshuvozeny019@gmail.com>
2025-08-22 23:34:27 +0600

    some code added

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -83,7 +83,7 @@ const services = [
     time: "3 hours setup",
   },
 ];
-// some code added and some also code added added some code 
+// sadded some code and some thing
 const Service = () => {
   const [expanded, setExpanded] = useState(null);
 
